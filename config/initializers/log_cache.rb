aee60386f75995a0539de4afb87f2160c1386f32
judahmeek <judah.meek@gmail.com>
2016-06-25 17:30:36 +0000

    Set up logging of all cache events

---
+++ config/initializers/log_cache.rb
@@ -0,0 +1,5 @@
+# config/initializers
+#ActiveSupport::Notifications.subscribe /active_support/ do |*args|
+#  event = ActiveSupport::Notifications::Event.new *args
+#  Rails.logger.info "Notification for event #{event.name} with payload #{event.payload}"
+#end
